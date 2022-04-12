d9b205d27ef01888601b882be62d5eca1e8a2743
LemonAndMint <orn.betul@gmail.com>
2022-04-12 16:53:40 +0300

    shell added
    shell added

---
+++ FinalProject/Assets/Scripts/bilgisayar/CMD/CMDInput.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CMDInput : MonoBehaviour
+{
+  InputField input;
+  InputField.OnChangeEvent changeEvent;
+  shell cmd;
+  void Start()
+  {
+    input = GetComponent<InputField>();
+    changeEvent = new InputField.OnChangeEvent();
+    cmd = new shell();
+
+    changeEvent.AddListener(SubmitName);
+    input.onValueChanged = changeEvent;
+  }
+  private void SubmitName(string args)
+  {
+    //Girilen tüm harfleri içinde tutar.
+    //#TODO: Zamanla birikecek bufferi temizle.
+    //#TODO: Boş karakter girildiğinin kontrolünü yap.
+    string[] words = args.Split('\n');
+    if (words[words.Length - 1] == "")
+		{
+      //Debug.Log(words[words.Length - 2]); //#FIXME ekranda ilk yazılan şey silinince OOB hatası veriyor
+      cmd.getCommand(words[words.Length - 2]);
+    }
+  }
+}
---
+++ FinalProject/Assets/Scripts/bilgisayar/CMD/shell.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+
+public class shell
+{
+	string[] parsdCmd; 
+  public void getCommand(string command)
+	{
+		parser(command);
+	}     
+
+	private void parser(string command)
+	{
+		parsdCmd = command.Split(' ');
+	}
+	
+	private void setch()
+	{
+
+	}
+}
